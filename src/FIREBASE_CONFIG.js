543b3a2d7d6a8dcd366fb99c887c4f2b0631d2f6
Brian Chow <32953270+khc25@users.noreply.github.com>
2022-01-28 21:47:33 +0800

    update

---
+++ src/FIREBASE_CONFIG.js
@@ -0,0 +1,18 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBf06bWQsZFmElZH9aRbmRSft_ZOWFoi0w",
+
+    authDomain: "tutor-b8e22.firebaseapp.com",
+  
+    databaseURL: "https://tutor-b8e22-default-rtdb.firebaseio.com",
+  
+    projectId: "tutor-b8e22",
+  
+    storageBucket: "tutor-b8e22.appspot.com",
+  
+    messagingSenderId: "685898082516",
+  
+    appId: "1:685898082516:web:45bbcad23732b8ab2bf341",
+  
+    measurementId: "G-JC088XEKQJ"
+  
+  };
